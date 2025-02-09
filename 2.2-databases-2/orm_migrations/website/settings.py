66c5138ab162a88787a2709a69eec8a547edae75
andrew.mamonow <andrew.mamonow@yandex.ru>
2025-02-09 22:33:06 +0300

    modified:   2.2-databases-2/m2m-relations/website/settings.py 	modified:   2.2-databases-2/orm_migrations/website/settings.py

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': '12qwaszx',
+        'PASSWORD': 'postgres',
         'OPTION': "-c client_encoding=utf8"
     }
 }
